7360102a83049196e9864e926951182be7d3fb73
mesamiked <love_bisphenol_a@yahoo.com>
2019-12-23 11:51:48 -0700

    Just a test.

--- wrangle-data.R
+++ wrangle-data.R
@@ -2,4 +2,6 @@ library(tidyverse)
 murders <- read_csv("data/murders.csv")
 murders <-murders %>% mutate(region = factor(region),
                              rate = total / population * 10^5)
-save(murders, file = "rdas/murders.rda")
+# save(murders, file = "rdas/murders.rda")
+
+# add a line to see if it can be pushed to git.
